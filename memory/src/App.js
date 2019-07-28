42b09369f10f2574b1152bcc47ebf46c16d47005
Katie Humburg <pookakat@gmail.com>
2019-07-27 23:31:28 -0400

    bang my head against the code for a while

--- memory/src/App.js
+++ memory/src/App.js
@@ -4,14 +4,26 @@ import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import Modal from "./components/Modal.js";
 
-const picsArray = ["20190330_161835_HDR.jpg", "20190410_112757_HDR.jpg", "20190518_182959.jpg", "IMG_20190724_094431.jpg", "IMG_20190703_100121.jpg", "IMG_20190714_100219.jpg", "IMG_20190714_100633.jpg", "IMG_20190726_202559.jpg", "IMG_20190716_163257.jpg", "IMG_20190722_100501.jpg", "IMG_20190724_094405.jpg", "IMG_20190726_202149.jpg"]
+var picsArray = [];
 
+export function newArray(){
+  picsArray = ["20190330_161835_HDR.jpg", "20190410_112757_HDR.jpg", "20190518_182959.jpg", "IMG_20190724_094431.jpg", "IMG_20190703_100121.jpg", "IMG_20190714_100219.jpg", "IMG_20190714_100633.jpg", "IMG_20190726_202559.jpg", "IMG_20190716_163257.jpg", "IMG_20190722_100501.jpg", "IMG_20190724_094405.jpg", "IMG_20190726_202149.jpg"]
+  for (var i = picsArray.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = picsArray[i];
+    picsArray[i] = picsArray[j];
+    picsArray[j] = temp;
+  };
+};
+
+newArray();
+ 
 function App(){
   return (
   <div id="container">
     <Modal />
     <Header />
-    <Memory picsArray={picsArray}/>
+    <Memory picsArray={picsArray} onChange="this.forceUpdate" />
     <Footer />
   </div>
   )
--- memory/src/components/Card.js
+++ memory/src/components/Card.js
@@ -1,9 +1,10 @@
 import React from "react";
+import {newArray} from "../App";
 
 export default function Card(props){
     return(
         <p>{props.picsArray.map(item => (
-            <button key={item.toString()} className="card-size"><img src={"./images/" + item}  className="pic-size" /></button>
+            <button key={item.toString()} onClick={newArray()} className="card-size"><img src={"./images/" + item} alt="Daisy" className="pic-size" /></button>
         ))}
         </p>
     );
